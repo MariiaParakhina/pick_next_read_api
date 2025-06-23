571b9c66b6f255c82aebe8c63e99b6d6a8638355
MariiaParakhina <parakhinam185@gmail.com>
2025-06-23 15:58:50 +0200

    add database scheme

---
+++ BookPickerAPI/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE booksDb;
+
+USE booksDb;
+
+CREATE TABLE books (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    title NVARCHAR(255) NOT NULL,
+    author NVARCHAR(255),
+    isPaper BIT,
+    status NVARCHAR(50)
+);
